fbc8ce1bd62e9544cfe96f62fd60f21417072582
Jeremias Enriquez <capuche777@gmail.com>
2024-12-03 22:32:24 -0600

    Add Product model with name, price, and quantity fields
    Implemented the Product model in both models.py and initial migration to handle product details in the database. The model includes fields for name, price, and quantity, all with default values ensuring data consistency. Additionally, a __str__ method was added for clear representation of product instances.

--- nimblestore/checkout/models.py
+++ nimblestore/checkout/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Product(models.Model):
-    # TODO Create your model here
-    # Add properties name, price and quantity
-    pass
+    name = models.CharField(max_length=255, null=True, blank=True, default=None)
+    price = models.FloatField(default=0)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
